742f33b5545dd85727e6f2b5920990da0af23f1a
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-08-11 22:24:38 -0300

    init

---
+++ Juego.cs
@@ -0,0 +1,43 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using SFML.Audio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiggerMax
+{
+    class Juego
+    {
+        private RenderWindow ventana;
+        public Juego() 
+        {
+            var modo = new VideoMode(800,600);
+            ventana = new RenderWindow(modo, "SFLM, funciona!!!");
+            ventana.KeyPressed += Ventana_PresionarTecla;//Levanta la tecla presionada?
+        }
+        public void Run() 
+        {
+            var circulo = new CircleShape(100f)
+            {
+                FillColor = SFML.Graphics.Color.Blue
+            };
+            //LOOP DEL JUEGO
+            while (ventana.IsOpen)
+            {
+                ventana.DispatchEvents();
+                ventana.Draw(circulo);
+                ventana.Display();
+            }
+        }
+        private void Ventana_PresionarTecla(object sender,KeyEventArgs e) 
+        {
+            if (e.Code == Keyboard.Key.Escape) 
+            {
+                Window window = (Window)sender;
+                window.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SFML.Window;
+using SFML.System;
+
+namespace DiggerMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Presiona Esc, para salir de la ventana");
+            Juego juego = new Juego();
+            Console.WriteLine("Todo, hecho!!!");
+            juego.Run();
+        }
+
+    }
+}
