e65464c5e7ddec540beecf22ffe67cc3b12e3b34
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-09-12 22:03:22 -0300

    clase BarrDeVida
    inicio falta desarrollar

---
+++ BarraDeSalud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiggerMax
+{
+    class BarraDeSalud
+    {
+        private float saludActual;
+        private float saludMaxima;
+        private enum EstadoDeSalud {Normal,Bajo,Critico};
+        private EstadoDeSalud actualEstadoDeSalud;
+        public BarraDeSalud()
+        {
+
+        }
+    }
+}
--- Enemigo.cs
+++ Enemigo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SFML.System;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Text;
